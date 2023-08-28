4ee5eb4492c40fb6d4ca9ebb706e2a10423759a6
akhil-06 <svam1008@gmail.com>
2023-08-28 21:54:32 +0530

    email subscriber project done

--- tailwindTut/tailwind.config.js
+++ tailwindTut/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./dist/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('https://images.unsplash.com/photo-1491497895121-1334fc14d8c9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8b290eXxlbnwwfDB8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60')",
+        'footer-texture': "url('/img/footer-texture.png')",
+      }
+    },
   },
   plugins: [],
 }
